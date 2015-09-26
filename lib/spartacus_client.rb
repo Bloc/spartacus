6b9b5445064f4a78611db51c22b44171422f1ac3
Ben Neely <ben@bloc.io>
2015-09-26 12:26:06 -0600

    Add checkpoint update route

--- lib/spartacus_client.rb
+++ lib/spartacus_client.rb
@@ -1,5 +1,57 @@
+require 'httparty'
+
 class SpartacusClient
-  def self.hi
-    puts "Hello world!"
+  include HTTParty
+
+  def initialize(api_jwt)
+    @api_jwt = api_jwt
+  end
+
+  # Set Base API Path
+  # @return [String] The base api path
+  def base_path
+    "http://localhost:3000/api/v1"
+  end
+
+  # Handle API timouts
+  def handle_timeouts
+    begin
+      yield
+    rescue Net::OpenTimeout, Net::ReadTimeout
+      {}
+    end
+  end
+
+  # Update a checkpoint
+  #
+  # @param id [Integer] A checkpoint id
+  # @param options [Hash] A customizable set of options.
+  # @option options [String] :name Checkpoint name.
+  # @option options [String] :summary Checkpoint summary.
+  # @option options [String] :body Checkpoint body.
+  # @option options [String] :assignment Checkpoint assignment.
+  # @option options [Integer] :points Checkpoint point.
+  # @return [HTTParty::Response] The updated checkpoint
+  # @example Update a checkpoint
+  #   SpartacusClient.new.update_checkpoint(129, {name: 'Real Cool Checkpoint'})
+  def update_checkpoint(id, options={})
+    accepted_params = [:name, :summary, :body, :assignment, :points]
+    checkpoint_params = options.select {|k, v| accepted_params.include?(k) }
+    update_checkpoint_url = "#{base_path}/checkpoints/#{id}"
+
+    handle_timeouts do
+      self.class.put(update_checkpoint_url,
+                     headers: auth_header,
+                     query: { checkpoint: checkpoint_params }).class
+    end
+  end
+
+  private
+
+  # Set the API Authorization Header
+  #
+  # @return [Hash] The authorization header
+  def auth_header
+    { "authorization" => @api_jwt }
   end
 end
--- spartacus_client.gemspec
+++ spartacus_client.gemspec
@@ -1,6 +1,7 @@
 Gem::Specification.new do |s|
   s.name        = 'spartacus_client'
-  s.version     = '0.0.0'
+  s.version     = '0.0.1'
+  s.add_runtime_dependency 'httparty', '~>0.13.7'
   s.date        = '2015-09-25'
   s.summary     = "A client for the Bloc AP"
   s.description = "A client for the Bloc API"
